805e21483532d1fbd1bbf40e302dbfd1e6f6c494
khaled <shnbyakuya@gmail.com>
2021-08-16 14:49:43 +0100

    added trash icon, style and functionality

--- app.js
+++ app.js
@@ -5,13 +5,20 @@ const tasks = document.querySelector(".tasks");
 
 const add = (e) => {
   e.preventDefault();
+  const task = document.createElement("p");
+  const remove = document.createElement("button");
   if (input.value != "") {
-    const todo = document.createElement("p");
-    todo.classList.add("task");
-    todo.innerText = input.value;
+    task.classList.add("task");
+    remove.classList.add("remove");
+    remove.innerHTML = '<i class="fas fa-trash"></i>';
+    task.innerText = input.value;
     input.value = "";
-    tasks.appendChild(todo);
+    task.appendChild(remove);
+    tasks.appendChild(task);
   }
+  remove.addEventListener("click", () => {
+    task.remove();
+  });
   input.focus();
 };
 
