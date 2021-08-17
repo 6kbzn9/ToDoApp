e98b5e2cb219a3ef43631bc81582350fc9aafc5f
khaled <shnbyakuya@gmail.com>
2021-08-17 09:38:11 +0100

    added save to local storage

--- app.js
+++ app.js
@@ -2,12 +2,34 @@
 const input = document.querySelector("input");
 const button = document.querySelector(".button");
 const tasks = document.querySelector(".tasks");
+let lists = [];
+
+if (localStorage.getItem("lists")) {
+  lists = JSON.parse(localStorage.getItem("lists"));
+  lists.forEach((list) => {
+    const task = document.createElement("p");
+    const remove = document.createElement("button");
+    task.classList.add("task");
+    remove.classList.add("remove");
+    remove.innerHTML = '<i class="fas fa-trash"></i>';
+    task.innerText = list;
+    task.appendChild(remove);
+    tasks.appendChild(task);
+    remove.addEventListener("click", () => {
+      task.remove();
+    });
+  });
+}
 
 const add = (e) => {
   e.preventDefault();
-  const task = document.createElement("p");
-  const remove = document.createElement("button");
-  if (input.value != "") {
+
+  if (input.value) {
+    lists.push(input.value);
+    localStorage.setItem("lists", JSON.stringify(lists));
+
+    const task = document.createElement("p");
+    const remove = document.createElement("button");
     task.classList.add("task");
     remove.classList.add("remove");
     remove.innerHTML = '<i class="fas fa-trash"></i>';
@@ -15,10 +37,13 @@ const add = (e) => {
     input.value = "";
     task.appendChild(remove);
     tasks.appendChild(task);
+
+    remove.addEventListener("click", () => {
+      task.remove();
+    });
   }
-  remove.addEventListener("click", () => {
-    task.remove();
-  });
+
+  console.log(lists);
   input.focus();
 };
 
